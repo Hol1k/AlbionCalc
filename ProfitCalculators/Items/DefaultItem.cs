71015907dbf25ca3dc40bfdaa20e11c7225ce2eb
Hol1k <danilugryumov96@gmail.com>
2024-04-24 10:45:46 +0500

    added .gitignore

---
+++ ProfitCalculators/Items/DefaultItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfitCalculators.Items
+{
+    internal class DefaultItem
+    {
+        string name = "";
+        byte tier = 1;
+        float weight = 0f;
+        int count = 0;
+
+        public DefaultItem() { }
+
+    }
+}
--- ProfitCalculators/RefiningCalculator.cs
+++ ProfitCalculators/RefiningCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProfitCalculators
 {
-    internal class RefiningCalculator
+    static internal class RefiningCalculator
     {
 
     }
